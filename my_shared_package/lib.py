fa9b009c554ebdcc1c2358d9d2ff242f6a7ed1d0
Eloisa Hernandez <eloisa.hdez.r@gmail.com>
2021-11-15 12:31:23 +0100

    Initial Push

---
+++ my_shared_package/lib.py
@@ -0,0 +1,10 @@
+def try_me():
+    print('Complete the verse: \n We will, we will...')
+    x = input()
+    if x == 'rock you':
+        print('yeah, you know your stuff')
+    else:
+        print('pff... try again')
+
+if __name__ == "__main__":
+    try_me()
